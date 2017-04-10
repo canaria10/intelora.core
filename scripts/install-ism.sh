b5eb8e251f0f2d50ae7f368933e229b4f720c635
Alexander Paul Quinit <paulquinit@gmail.com>
2017-04-10 17:39:41 +0800

    fixed ism issue

--- scripts/install-ism.sh
+++ scripts/install-ism.sh
@@ -7,7 +7,7 @@ cd mycroft
 rm -rf ism
 git clone https://github.com/intelora/intelora.skill.manager.git ism
 chmod +x ism/ism
-
+cd ..
 if [[ ${IS_TRAVIS} != true ]]; then
     echo "Create /opt/mycroft/skills if it doesn't exist"
     if [ ! -d ${SKILLS_DIR} ]; then
